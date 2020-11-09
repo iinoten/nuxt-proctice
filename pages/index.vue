74a90e83359362155533ceff0ffa3acec1229f6c
Iino Ten <nteten0313@icloud.com>
2020-11-10 03:05:05 +0900

    ボタンを押してDOMが更新されるようにした

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,38 @@
 <template>
-<div class="page-index">
-    <section class="about">
-      <h2>夏目漱石</h2>
-      <div class="article-body">
-        <img src="~/assets/images/portrait.png" alt="夏目漱石">
-        <p>日本の小説家、評論家、英文学者。本名、夏目 金之助（なつめ きんのすけ）。江戸の牛込馬場下横町（現在の東京都新宿区喜久井町）出身。俳号は愚陀仏。詳細は<a href="https://ja.wikipedia.org/wiki/夏目漱石" target="_blank">ウィキペディア</a>を。</p>
-      </div>
-    </section>
-
-    <section class="works">
-      <h2>代表作</h2>
-      <ArticleBody>
-        <p>『吾輩は猫である』（1905年）がデビュー作である。</p>
-        <ul>
-          <li>『吾輩は猫である』（1905年）</li>
-          <li>『坊っちゃん』（1906年）</li>
-          <li>『草枕』（1906年）</li>
-          <li>『三四郎』（1908年）</li>
-        </ul>
-      </ArticleBody>
-    </section>
+  <div>
+    <h1>table page</h1>
+    <button @click="fetch">fetch</button>
+    <p>{{ items }}</p>
   </div>
 </template>
 
+<script>
+  function getJson() {
+    const ages = Array.from(
+      new Set(
+        new Array( parseInt(Math.random() * 100))
+          .fill( null )
+          .map(() => parseInt(Math.random() * 100))
+      )
+    )
+    return ages.map(age => ({
+      age,
+      users: Math.random() * 10000
+    }))
+  }
+
+  export default {
+  data: () => ({
+    items: []
+  }),
+
+  methods: {
+    fetch() {
+      this.items = getJson()
+    }
+  }
+}
+</script>
 <style scoped lang="scss">
 
   .about,
