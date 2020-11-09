5b21b4c03258bf1e487ba60ee5b75599e171fd24
Iino Ten <nteten0313@icloud.com>
2020-11-10 00:02:19 +0900

    404ページの追加

---
+++ layouts/error.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="layout-error">
+    <section class="error">
+      <h2>error</h2>
+      <div class="article-body">
+        <h3>名前はまだない</h3>
+        <p>アクセスしたページは存在しないか、すでに削除されています。</p>
+      </div>
+    </section>
+  </div>
+</template>
