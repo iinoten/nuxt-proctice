98af7364817f9cb65f761257c6a97415daf07ef9
Iino Ten <nteten0313@icloud.com>
2020-11-10 00:16:29 +0900

    scssを適用

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,6 +16,8 @@ export default {
   },
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
+    'reset-css',
+    '~/assets/scss/style.scss'
   ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
