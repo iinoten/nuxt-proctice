02a5b1db522f00c192679678cc7423423dca95bc
Iino Ten <nteten0313@icloud.com>
2020-11-10 02:04:28 +0900

    スタイルをコンポーネント内で定義

--- components/common/TheHeader.vue
+++ components/common/TheHeader.vue
@@ -5,3 +5,24 @@
     </div>
   </header>
 </template>
+
+<style scoped lang="scss">
+  @import "~assets/scss/variables";
+
+  .header {
+    background-color: $color-gray;
+  }
+
+  .header-inner {
+    height: 60px;
+    max-width: 600px;
+    margin: 0 auto;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    a {
+      color: inherit;
+    }
+  }
+</style>
