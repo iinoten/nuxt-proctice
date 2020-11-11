50602cd2237969a9cd4bef4df142b375b56d73df
Iino Ten <nteten0313@icloud.com>
2020-11-12 06:44:30 +0900

    チャットのログを表示

--- pages/chat/index.vue
+++ pages/chat/index.vue
@@ -1,16 +1,36 @@
 <template>
-  <div>chat page</div>
+  <div>
+    <div>chat page</div>
+    <div>
+      <ul>
+        <li v-for="item in this.chatLogs" :key="item.message">
+          {{ item.message }}
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
   import firebase from '~/plugins/firebase'
-  const db = firebase.firestore()
-  db.collection('chats').doc('test')
-       .get()
-       .then((res) => {
-         console.log(res.data())
-       })
   export default {
-    
+    data: ()=>({
+      chatLogs: []
+    }),
+    mounted: function () {
+       const db = firebase.firestore()
+        db.collection('chats').doc('test')
+          .get()
+          .then((res) => {
+            console.log(res.data())
+            const chatLogsDataArray = res.data().logs;
+            const tempChatLog = []
+              chatLogsDataArray.forEach(element => {
+                tempChatLog.push(element)
+              });
+              console.log(tempChatLog)
+              this.chatLogs = tempChatLog
+          })
+    }
   }
 </script>
